ee60be69d2dd52cb4c854d0ec34fbf87ad72cd74
Dev Harsh <devharsh466@gmail.com>
2025-02-16 19:40:06 +0530

    Update page.tsx

--- client/src/app/view-tree/page.tsx
+++ client/src/app/view-tree/page.tsx
@@ -1,23 +1,22 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-
-interface Node {
+interface Node<T = unknown> {
   key: string;
-  value: any;
-  left: Node | null;
-  right: Node | null;
+  value: T;
+  left: Node<T> | null;
+  right: Node<T> | null;
 }
 
 const ViewTree = () => {
-  const [treeData, setTreeData] = useState<Node | null>(null);
+  const [treeData, setTreeData] = useState<Node<string | number> | null>(null);
 
   useEffect(() => {
     const fetchTree = async () => {
       try {
         const response = await fetch("https://file-search-ecru.vercel.app/show-tree"); // Ensure the route matches your backend API
         const data = await response.json();
-        setTreeData(data['data']);
+        setTreeData(data["data"]);
       } catch (error) {
         console.error("Error fetching tree data:", error);
       }
@@ -26,7 +25,7 @@ const ViewTree = () => {
     fetchTree();
   }, []);
 
-  const renderTree = (node: Node | null) => {
+  const renderTree = (node: Node<string | number> | null) => {
     if (!node) {
       return (
         <div style={{ marginLeft: "20px" }}>
